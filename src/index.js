86dd0151d1cadd895cc50fcd91bec962b5dba513
root <chrismeekymin@min.ng>
2020-11-24 09:37:25 -0500

    set up express server

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.all('*', (req, res) => res.status(404).json({
+  success: false,
+  message: 'You are on Investico NIG LTD but it looks like The page you are looking for does not exist'
+}));
+
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, console.info(`server started on port ${PORT}`));
+
+export default app;
